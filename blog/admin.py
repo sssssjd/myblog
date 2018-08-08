2a9f845aac275f216946db3172adac73bd3fc076
sssssjd <sssssjd@163.com>
2018-08-08 19:04:51 +0800

    课程完成

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from blog.models import Article
 # Register your models here.
 
-admin.site.register(Article)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'pub_time',)
+    list_filter = ('pub_time',)
+    pass
+
+
+admin.site.register(Article, ArticleAdmin)
--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,8 @@ from django.urls import path, re_path
 from . import views
 urlpatterns = [
     path('', views.index),
-    re_path(r'^article/(?P<article_id>[0-9]+)$', views.article_page),
+    re_path(r'^article/(?P<article_id>[0-9]+)$', views.article_page, name = 'article_page'),
+    re_path(r'^edit/(?P<article_id>[0-9]+)$', views.edit_page, name = 'edit_page'),
+    re_path(r'^edit/action$', views.edit_action, name = 'edit_action'),
 ]
+app_name='blog'
